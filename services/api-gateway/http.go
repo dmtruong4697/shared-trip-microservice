467707857d9e42ad2360804d4e967d5d82afc45c
dmtruong4697 <truonggduonggmadridista@gmail.com>
2025-10-29 23:37:26 +0700

    ss7 43

--- services/api-gateway/http.go
+++ services/api-gateway/http.go
@@ -1,12 +1,40 @@
 package main
 
 import (
-	"bytes"
 	"encoding/json"
 	"log"
 	"net/http"
+	"ride-sharing/services/api-gateway/grpc_clients"
 	"ride-sharing/shared/contracts"
 )
+
+func handleTripStart(w http.ResponseWriter, r *http.Request) {
+	var reqBody startTripRequest
+	if err := json.NewDecoder(r.Body).Decode(&reqBody); err != nil {
+		http.Error(w, "failed parse Json data", http.StatusBadRequest)
+		return
+	}
+
+	defer r.Body.Close()
+
+	tripService, err := grpc_clients.NewTripServiceClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tripService.Close()
+
+	trip, err := tripService.Client.CreateTrip(r.Context(), reqBody.ToProto())
+	if err != nil {
+		log.Printf("failed to start trip %v", err)
+		http.Error(w, "failed to start trip", http.StatusInternalServerError)
+		return
+	}
+
+	response := contracts.APIResponse{Data: trip}
+
+	writeJSON(w, http.StatusCreated, response)
+
+}
 
 func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 	// if r.Method != http.MethodPost {
@@ -28,25 +56,22 @@ func handleTripPreview(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	jsonBody, _ := json.Marshal(reqBody)
-	reader := bytes.NewReader(jsonBody)
+	tripService, err := grpc_clients.NewTripServiceClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer tripService.Close()
+
+	// tripService.Client.PreviewTrip()
 
 	//TODO: Call trip service
-	res, err := http.Post("http://trip-service:8083/preview", "application/json", reader)
+	tripPreview, err := tripService.Client.PreviewTrip(r.Context(), reqBody.ToProto())
 	if err != nil {
-		log.Println(err)
+		log.Printf("failed to preview trip %v", err)
+		http.Error(w, "failed to preview trip", http.StatusInternalServerError)
 		return
 	}
-
-	defer res.Body.Close()
-
-	var respBody any
-	if err := json.NewDecoder(res.Body).Decode(&respBody); err != nil {
-		http.Error(w, "failed parse Json data from trip-service", http.StatusBadRequest)
-		return
-	}
-
-	response := contracts.APIResponse{Data: respBody}
+	response := contracts.APIResponse{Data: tripPreview}
 
 	writeJSON(w, http.StatusCreated, response)
 }
--- services/api-gateway/types.go
+++ services/api-gateway/types.go
@@ -1,6 +1,9 @@
 package main
 
-import "ride-sharing/shared/types"
+import (
+	pb "ride-sharing/shared/proto/trip"
+	"ride-sharing/shared/types"
+)
 
 type previewTripRequest struct {
 	UserID      string           `json:"userID"`
@@ -11,4 +14,30 @@ type previewTripRequest struct {
 // Read implements io.Reader.
 func (previewTripRequest) Read(p []byte) (n int, err error) {
 	panic("unimplemented")
+}
+
+func (p *previewTripRequest) ToProto() *pb.PreviewTripRequest {
+	return &pb.PreviewTripRequest{
+		UserID: p.UserID,
+		StartLocation: &pb.Coordinate{
+			Latitude:  p.Pickup.Latitude,
+			Longitude: p.Pickup.Longitude,
+		},
+		EndLocation: &pb.Coordinate{
+			Latitude:  p.Destination.Latitude,
+			Longitude: p.Pickup.Longitude,
+		},
+	}
+}
+
+type startTripRequest struct {
+	RideFareID string `json:"rideFareID"`
+	UserID     string `json:"userID"`
+}
+
+func (c *startTripRequest) ToProto() *pb.CreateTripRequest {
+	return &pb.CreateTripRequest{
+		RideFareID: c.RideFareID,
+		UserID:     c.UserID,
+	}
 }
